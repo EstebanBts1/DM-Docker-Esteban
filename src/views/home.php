04ffa6d5972ca624b5da157d2bd29cbe2b8faad4
EstebanBts1 <estebanchaussinand@gmail.com>
2025-05-20 11:07:38 +0200

    ajout css

--- src/views/home.php
+++ src/views/home.php
@@ -1,6 +1,14 @@
-<h1>Bienvenue dans l'application de Gestion de Stock</h1>
-<ul>
+<head>
+  <meta charset="UTF-8">
+  <title>Application de Gestion de Stock</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<div class="container">
+  <h1>Bienvenue dans l'application de Gestion de Stock</h1>
+  <ul>
     <li><a href="?controller=produit&action=index">Produits</a></li>
     <li><a href="?controller=fournisseur&action=index">Fournisseurs</a></li>
     <li><a href="?controller=commande&action=index">Commandes</a></li>
-</ul>
+  </ul>
+</div>
